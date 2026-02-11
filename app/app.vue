b41c854afbb3622ed1c566b56134b491f919bdf7
Goodness Urama <goodyboy301@gmail.com>
2026-02-11 06:55:29 +0100

    feat: add project metadata for SEO optimization

--- app/app.vue
+++ app/app.vue
@@ -16,6 +16,37 @@ import Lenis from "@studio-freight/lenis";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
+const projName = ref(`WhatWhatGoodyDo?`);
+const projDesc = ref(`fragments of my imagination exposed to the light`);
+const projImg = ref(`https://cdn.whatwouldgoodydo.com/12-principles-of-animations.webp`);
+
+const computedPageMeta = computed(() => {
+  return {
+    title: projName.value,
+    meta: [
+      { hid: "description", property: "description", content: projDesc.value },
+      { hid: "og-type", property: "og:type", content: "website" },
+      { hid: "og-title", property: "og:title", content: projName.value },
+      { hid: "og-description", property: "og:description", content: projDesc.value },
+      {
+        hid: "og-image",
+        property: "og:image",
+        content: projImg,
+      },
+      { hid: "twitter-card", property: "twitter:card", content: "summary_large_image" },
+      { hid: "twitter-title", property: "twitter:title", content: projName.value },
+      { hid: "twitter-description", property: "twitter:description", content: projDesc.value },
+      {
+        hid: "twitter-image",
+        property: "twitter:image",
+        content: projImg,
+      },
+    ],
+  };
+});
+
+useHead(computedPageMeta);
+
 onMounted(() => {
   const lenis = new Lenis({
     lerp: 0.1,
